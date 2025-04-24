0b4dff5de2edaaf101f4dfb81b59d2bf7aa7bed6
LlanderSei <seiranruroku@gmail.com>
2025-04-24 21:01:06 +0800

    2nd Lab Exam (Partial)

---
+++ 2ndLabExam/database/migrations/2025_04_24_120057_create_tasks_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('TaskID');
+            $table->string('Title');
+            $table->text('Description');
+            $table->boolean('IsCompleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
